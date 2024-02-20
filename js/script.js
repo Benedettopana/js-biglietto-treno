91e59eca5122b783c1282fb38bc996de0faa12a6
Benedettopana <benedetto.pana@gmail.com>
2024-02-20 14:24:44 +0100

    [add]sconto

--- js/script.js
+++ js/script.js
@@ -4,8 +4,23 @@ const rate = 0.21;
 
 // calcolo prezzo:
 let ticketPrice = parseFloat(myKm * rate);
-ticketPrice = ticketPrice.toFixed(2)
+ticketPrice = ticketPrice.toFixed(2);
 console.log(myKm, myAge, ticketPrice);
+let message = 'Non è stato applicato nessuno sconto.';
+let sale = 0;
+if(myAge <= 18){
+  sale = (ticketPrice * 20) / 100;
+  message = 'È stato applicato lo sconto del 20%.';
+} 
+  
+else if (myAge >= 65){
+  sale = (ticketPrice * 40) / 100;
+  message = 'È stato applicato lo sconto del 40%.';
+} 
+console.log(sale);
+ticketPrice -= sale;
+ticketPrice = ticketPrice.toFixed(2);
+console.log(ticketPrice);
 
 
 
